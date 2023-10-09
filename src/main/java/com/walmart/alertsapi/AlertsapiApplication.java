16b23a3f040a80ed3e473551f2f036b2b2c87041
haoranhu1993 <haoranhu@umass.edu>
2023-10-09 18:53:53 -0400

    start the project

---
+++ src/main/java/com/walmart/alertsapi/AlertsapiApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.alertsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/alertsapi/AlertsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.alertsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
